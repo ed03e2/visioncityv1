d468d4e1c94046c11ce4d1f52a22aa474d86610e
Ivan Romero <i.wells.ar@gmail.com>
2025-02-17 21:22:11 -0600

    add endpoint and front for zone durations and some memo at frontend, optional tho

--- src/frontend/node_modules/@deck.gl/react/dist/utils/use-widget.d.ts
+++
@@ -1,5 +0,0 @@
-import { type Widget } from '@deck.gl/core';
-export declare function useWidget<T extends Widget, PropsT extends {}>(WidgetClass: {
-    new (props: PropsT): T;
-}, props: PropsT): T;
-//# sourceMappingURL=use-widget.d.ts.map
